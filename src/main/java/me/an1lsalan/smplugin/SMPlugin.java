8398b7f840b7c7648494f44f245a89f54ff66218
Anil Can MF Salan <76490030+an1lsalan@users.noreply.github.com>
2022-04-30 23:54:34 +0200

    #0002 Event Listener 1
    Message für Server Beitritt und Bett Verlassen

--- src/main/java/me/an1lsalan/smplugin/SMPlugin.java
+++ src/main/java/me/an1lsalan/smplugin/SMPlugin.java
@@ -1,17 +1,33 @@
 package me.an1lsalan.smplugin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SMPlugin extends JavaPlugin {
+public final class SMPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Plugin enabled");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         System.out.println("Plugin disabled");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(event.getPlayer().getDisplayName() + " ist dem verdammten Server beigetreten!");
+    }
+
+    @EventHandler
+    public void onLeaveBed(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage(event.getPlayer().getDisplayName() +",warum verlässt du dein Bett?");
     }
 }
